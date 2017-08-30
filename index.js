c5791222150d9fd3f8c50427d868eb3f85a2f5d0
osboxes <osboxes@osboxes>
2017-08-30 16:50:38 +0100

    Prueba de conexión

--- index.js
+++ index.js
@@ -1,18 +1,58 @@
 'use strict';
 
-const express = require('express');
-const bodyParser = require('body-parser');
+var configFile=require('./config');
 
-const restService = express();
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 
-restService.use(bodyParser.urlencoded({
+//connection to mongo
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk(configFile.mongoURL);
+
+
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var app=express();
+//view engine setup
+app.set('views', path.join(dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+
+app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-restService.use(bodyParser.json());
+app.use(bodyParser.json());
 
-restService.post('/echo', function(req, res) {
+//Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+
+app.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    
+    	
+    	
     return res.json({
         speech: speech,
         displayText: speech,
@@ -20,7 +60,7 @@ restService.post('/echo', function(req, res) {
     });
 });
 
-restService.post('/slack-test', function(req, res) {
+app.post('/slack-test', function(req, res) {
 
     var slack_message = {
         "text": "Details of JIRA board for Browse and Commerce",
@@ -76,6 +116,6 @@ restService.post('/slack-test', function(req, res) {
 
 
 
-restService.listen((process.env.PORT || 8000), function() {
+app.listen((process.env.PORT || 8000), function() {
     console.log("Server up and listening");
 });
