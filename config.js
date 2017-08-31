b0f0a9af66f41c0da31f1cde2009d43834f9b84a
ax30376 <ax30376@PC0AQCC5.axa-seguros-es.intraxa>
2017-08-31 18:41:23 +0200

    XX

--- config.js
+++ config.js
@@ -1,2 +1,1 @@
-module.exports.mongoURL="ncbot17:efedlb17#@ds111124mlab.com:11124/quinoa";
-module.exports.mongoURL2="ncbot17:efedlb17#@ds111124mlab.com:11124/quinoa";
+module.exports.mongoURL="ncbot17:efedlb17#@ds111124mlab.com:11124/quinoa"
