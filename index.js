cdd0145229f6b5785bcdc2899e6a258f88113f0d
SoreCer <45244691+SoreCer@users.noreply.github.com>
2018-11-30 13:17:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,24 @@ restService.use(bodyParser.json());
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     //var speech = "Hola Jorge";
+    //var speech = "Hola santi";
+    /*var type = req.body.result && req.body.result.parameters && req.body.result.parameters.TypeOfQuote;
+    switch (type){
+        case "felicidad":
+             var speech ="Aquellos que quieren cantar siempre encuentran una canción";
+            break;
+        case "amor":
+             var speech ="No hay amor imposible en esta vida, lo que sí existe son las personas que desisten justo antes de alcanzar el verdadero amor";
+            break;
+        case "amistad":
+            var speech ="Lo amigos son la familia que se escoge";
+            break;
+        case "inspiracion":
+            var speech ="Solo los que corren el riesgo de avanzar, pueden saber hasta dónde pueden llegar";
+            break;
+        default:
+            var speech ="El destino mezcla las cartas, y nosotros las jugamos";
+    }*/
     return res.json({
         speech: speech,
         displayText: speech,
