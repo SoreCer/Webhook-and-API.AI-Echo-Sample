16aabc59626717a9c33dd5a33b3c7c3f24941fed
SoreCer <45244691+SoreCer@users.noreply.github.com>
2018-11-30 13:18:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ restService.use(bodyParser.json());
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     //var speech = "Hola Jorge";
-    //var speech = "Hola santi";
+    var speech = "Hola santi";
     /*var type = req.body.result && req.body.result.parameters && req.body.result.parameters.TypeOfQuote;
     switch (type){
         case "felicidad":
